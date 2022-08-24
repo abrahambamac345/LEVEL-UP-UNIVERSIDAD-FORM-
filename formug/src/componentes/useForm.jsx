e8f15b5735174357fb5739c8e6480b7183254da4
abrahambamac345 <104279106+abrahambamac345@users.noreply.github.com>
2022-08-24 00:39:35 -0600

    formsinb

--- formug/src/componentes/useForm.jsx
+++ formug/src/componentes/useForm.jsx
@@ -1,49 +1,52 @@
-import { useState, useEffect  } from "react";
+import { useState, useEffect } from "react";
 
+ let participantes = JSON.parse(localStorage.getItem("values"));
+const useForm = (callback, validacion) => {
+ 
+  const [participantes, setParticipantes] = useState(participantes || []);
 
-const useForm = (callback, validacion ) => {
-    const [values, setValues]= useState ( {
-            carné: "",
-            nombre: "",
-            dirección: "",
-            genero: "",
-            teléfono: "",
-            fecha: "", 
-            carrera: "",
-            poesía:"",
-    
+  const [values, setValues] = useState({
+    carné: "",
+    nombre: "",
+    dirección: "",
+    genero: "",
+    teléfono: "",
+    fecha: "",
+    carrera: "",
+    poesía: "",
+
+  });
+  const [errors, setErrors] = useState({});
+ const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value
     });
-    const [errors, setErrors] = useState({});
-    const [isSubmitting, setIsSubmitting] = useState(false);
-  
-    const handleChange = e => {
-        const { name, value } = e.target;
-        setValues({
-          ...values, 
-          [name]: value
-        });
-      };
-    
-      const handleSubmit = e => {
-        e.preventDefault();
-        setErrors(validacion(values));
-        setIsSubmitting(true);
-    
-      };
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setErrors(validacion(values));
+    setIsSubmitting(true);
+    setParticipantes(...participantes, values);
+  };
 
 
-      useEffect(
-        () => {
-        localStorage.setItem('values',JSON.stringify(values));
+  useEffect(
+    () => {
+      if (Object.keys(errors).length === 0 && isSubmitting) {
+        callback();
+         localStorage.setItem('values', JSON.stringify(participantes));
+      }
+     
+    }
+  );
 
-         if (Object.keys(errors).length === 0 && isSubmitting) {
-          callback();
-          }
-        }
-        );
+  return { handleChange, handleSubmit, values, errors };
+};
 
-      return { handleChange, handleSubmit, values, errors};
-       };
-    
-       
+
 export default useForm;
